4eee933b6819e110c2628cc244e74098ae8569b5
hhyewon <hiimhyewon@naver.com>
2021-03-08 19:33:04 +0900

    예외처리

---
+++ DoitJava/Chapter14/exception/ExceptionTest.java
@@ -0,0 +1,45 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionTest {
+	/*
+	public static void main(String[] args) {
+
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream("a.txt");
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				fis.close(); //입출력을 열면 닫아줘야됨
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			System.out.println("finally");
+		}
+		System.out.println("end");
+	}
+	/*
+	java.io.FileNotFoundException: a.txt (지정된 파일을 찾을 수 없습니다)
+	Exception in thread "main" java.lang.NullPointerException: Cannot invoke "java.io.FileInputStream.close()" because "fis" is null
+		at exception.ExceptionTest.main(ExceptionTest.java:19)
+	* a.txt를 왜 못찾지
+	*/
+	public static void main(String[] args) {
+	
+		try (FileInputStream fis = new FileInputStream("a.txt")){
+		
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		System.out.println("end");
+	}
+	}
+	
+	
